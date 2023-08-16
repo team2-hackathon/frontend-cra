2b56ec8a68960ca1d89165c45e0ba1d1b855ae3d
Stephen Wong <wongstephenky@gmail.com>
2023-08-16 10:44:15 -0700

    updated nav

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,8 +1,8 @@
 export const NavBar = () => {
   return (
-    <div className="bg-yellow-100">
+    <div className="bg-yellow-100 text-lg font-bold">
       This is the NavBar. Below are links
-      <ul className="flex gap-4">
+      <ul className="flex gap-4 ">
         <li>
           <a href="/">Home</a>
         </li>
