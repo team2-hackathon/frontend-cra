475461ae8f097c80b2419affdfc5065b97e4d38b
Stephen Wong <wongstephenky@gmail.com>
2023-08-16 19:27:55 -0700

    added nav items

--- src/App.js
+++ src/App.js
@@ -14,20 +14,22 @@ if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
 const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
 function App() {
   return (
-    <>
-      <NavBar />
-
-      <ClerkProvider publishableKey={clerkPubKey}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/logout" elemen={<Logout />} />
-          <Route element={<ProtectedPage />}>
-            <Route path="/dashboard" element={<DashboardPage />} />
-          </Route>
-        </Routes>
-      </ClerkProvider>
-    </>
+    <div className="bg-gray-800 min-h-fullscreen min-w-">
+      {/* limits the size of the app to 384 */}
+      <div className="max-w-sm min-h-screen mx-auto bg-white">
+        <NavBar />
+        <ClerkProvider publishableKey={clerkPubKey}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/logout" elemen={<Logout />} />
+            <Route element={<ProtectedPage />}>
+              <Route path="/dashboard" element={<DashboardPage />} />
+            </Route>
+          </Routes>
+        </ClerkProvider>
+      </div>
+    </div>
   );
 }
 
